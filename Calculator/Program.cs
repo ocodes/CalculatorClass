7a0701d975cb16c51439557f9c1ee31c33802104
omar <omar@omars-air.lan>
2015-07-13 14:07:36 -0400

    Work In Progress

---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string sExpression  = "8*5+9/3"; // (((((((1+2.5)*(0-4))*2)/9)*9)/5)-2)+(8+(7/(8*5)))
+			Console.WriteLine ( CalculatorClass.evaluateExpression( sExpression ));
+
+			sExpression = "(8*5)+(9/3)";
+			Console.WriteLine ( CalculatorClass.evaluateExpression( sExpression ));
+		}
+		 
+	}
+}
